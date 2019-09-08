e1dc3881badd5e36eb08bb3aec50dc901466297a
tgmeng <tgmeng@163.com>
2019-09-08 19:30:34 +0800

    feat: base-generator

---
+++ generators/base/index.js
@@ -0,0 +1,22 @@
+
+const Generator = require('../base-generator');
+
+module.exports = class extends Generator {
+  configuring() {
+    [
+      '.editorconfig',
+      '.eslintrc.js',
+      '.gitignore',
+    ].forEach((file) => {
+      this.fs.copy(this.templatePath(file), this.destinationPath(file));
+    });
+  }
+
+  writing() {
+    this._updatePackageJSON();
+  }
+
+  install() {
+    this.yarnInstall();
+  }
+};
