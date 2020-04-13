38e5af75bd6430e02b5baa2d1b85084cfd5b450c
tgmeng <tgmeng@163.com>
2020-04-13 10:31:34 +0800

    config: 项目, 增加 prettier & 完善 eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,17 @@
 module.exports = {
-  extends: ['eslint:recommended', 'airbnb-base'],
   env: {
     jest: true,
-    node: true,
+    node: true
   },
+  extends: [
+    'eslint:recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'airbnb-base',
+    'prettier'
+  ],
+  plugins: ['import'],
   rules: {
-    'no-underscore-dangle': 0,
-  },
+    'no-underscore-dangle': 0
+  }
 };
